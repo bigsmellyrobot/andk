adc731ccac9a8b15d5d38040eef0e3c0f66ed7b6
Andrew Kennedy <ak@bigsmellyrobot.com>
2020-06-18 16:46:58 -0500

    dark/light theme updates

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.font-sans.text-white.bg-grey-darkest.min-h-screen.pb-8
+.font-sans.min-h-screen.pb-8
   site-header
   nuxt
 </template>
@@ -14,12 +14,45 @@ export default {
 }
 </script>
 
-<style lang="sass">
-body
-  max-width: 100vw
-  overflow-x: hidden
-.text-justify p:not(:first-child)
+<style lang="scss">
+@import 'assets/css/settings.scss';
+body {
+  max-width: 100vw;
+  overflow-x: hidden;
+  background: $gray;
+
+  &, a {
+    color: $white;
+  }
+
+  .gray-95 {
+    background: $gray-95;
+  }
+
+  .nav-btn {
+    border-color: $white;
+  }
+
+  @media (prefers-color-scheme: light) {
+    background: $white2;
+    &, a {
+      color: $gray;
+    }
+    .gray-95 {
+      background: $white-95;
+    }
+    .nav-btn {
+      border-color: $gray;
+    }
+  }
+}
+.text-justify p:not(:first-child) {
   text-indent: 2rem
-a
+}
+a {
   transition: color .25s, border-width .25s
+}
+.gray-95 {
+  background: $gray-95;
+}
 </style>
